1d0d0fb628e176ce0fd68960805dace739d49db6
naonao <276286687@qq.com>
2024-12-07 23:11:49 +0800

    12/7-2

--- somefunctions1.c
+++ somefunctions1.c
@@ -97,7 +97,20 @@ ll lcm (ll a, ll b)
 {
     return (a / gcd(a,b)) * b;
 }
-
+long long fastPower(long long base, long long power, long long mod)
+{
+    long long result = 1;
+    while(power > 0)
+    {
+        if(power & 1)
+        {
+            result = result * base % mod;
+        }
+        power >>= 1;
+        base = (base * base) % mod;
+    }
+    return result;
+}
 int main()
 {
     int a, b, c;
